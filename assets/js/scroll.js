cebce3d5167d8d95106e84ac237a27b21e9725de
DaniekTj <imperio.hacker@gmail.com>
2023-04-03 18:25:26 -0700

    text update

--- assets/js/scroll.js
+++ assets/js/scroll.js
@@ -1,58 +1,65 @@
 window.addEventListener('load', function() {
   
-  var monancreP = document.querySelector('#monancre p:first-child');
-  var scrollToMonancreP = monancreP.getBoundingClientRect().top + window.pageYOffset;
-  
-  // Ajusta esta velocidad a tu preferencia
-  var scrollSpeed = 1; // 1 pixel por iteración
-  var scrollInProgress = false; // Variable para controlar el desplazamiento en progreso
-  
-  function customSmoothScroll() {
-    var currentScroll = window.pageYOffset;
-    if (currentScroll < scrollToMonancreP) {
-      window.scrollTo(0, currentScroll + scrollSpeed);
-      requestAnimationFrame(customSmoothScroll);
-    } else {
-      scrollInProgress = false;
+    var monancreP = document.querySelector('#monancre p:first-child');
+    var scrollToMonancreP = monancreP.getBoundingClientRect().top + window.pageYOffset;
+    
+    // Ajusta esta velocidad a tu preferencia
+    var scrollSpeed = 1; // 1 pixel por iteración
+    var scrollInProgress = false; // Variable para controlar el desplazamiento en progreso
+    
+    function customSmoothScroll() {
+      var currentScroll = window.pageYOffset;
+      if (currentScroll < scrollToMonancreP) {
+        window.scrollTo(0, currentScroll + scrollSpeed);
+        requestAnimationFrame(customSmoothScroll);
+      } else {
+        scrollInProgress = false;
+      }
     }
-  }
-  
-  function runCustomSmoothScroll() {
-    if (!scrollInProgress && window.pageYOffset < scrollToMonancreP) {
-      scrollInProgress = true;
-      customSmoothScroll();
+    
+    function runCustomSmoothScroll() {
+      if (!scrollInProgress && window.pageYOffset < scrollToMonancreP) {
+        scrollInProgress = true;
+        customSmoothScroll();
+      }
     }
-  }
-  
-  // Utilizamos debounce para retrasar la ejecución de la función
-  function debounce(func, wait) {
-    var timeout;
-    return function() {
-      var context = this, args = arguments;
-      var later = function() {
-        timeout = null;
-        func.apply(context, args);
+    
+    // Utilizamos debounce para retrasar la ejecución de la función
+    function debounce(func, wait) {
+      var timeout;
+      return function() {
+        var context = this, args = arguments;
+        var later = function() {
+          timeout = null;
+          func.apply(context, args);
+        };
+        clearTimeout(timeout);
+        timeout = setTimeout(later, wait);
       };
-      clearTimeout(timeout);
-      timeout = setTimeout(later, wait);
     };
-  };
-  
-
-  
-  setInterval(function() {
-    // Ejecutamos el desplazamiento suave si el scroll está en la parte superior y no hay un desplazamiento manual en progreso
-    if (window.pageYOffset === 0 && !scrollInProgress) {
-      runCustomSmoothScroll();
-    }
-  }, 16);
-  
-  // Detener el desplazamiento suave al hacer clic o tocar la pantalla
-  window.addEventListener('click', function() {
-    scrollInProgress = false;
+    
+    // Ejecutamos el desplazamiento suave al final del debounce
+    var debounceCustomSmoothScroll = debounce(runCustomSmoothScroll, 50);
+    
+    window.addEventListener('scroll', function() {
+      // Llamamos al debounce al realizar un desplazamiento manual
+      debounceCustomSmoothScroll();
+    });
+    
+    setInterval(function() {
+      // Ejecutamos el desplazamiento suave si el scroll está en la parte superior y no hay un desplazamiento manual en progreso
+      if (window.pageYOffset === 0 && !scrollInProgress) {
+        runCustomSmoothScroll();
+      }
+    }, 16);
+    
+    // Detener el desplazamiento suave al hacer clic o tocar la pantalla
+    window.addEventListener('click', function() {
+      scrollInProgress = false;
+    });
+    window.addEventListener('touchstart', function() {
+      scrollInProgress = false;
+    });
+    
   });
-  window.addEventListener('touchstart', function() {
-    scrollInProgress = false;
-  });
   
-});
